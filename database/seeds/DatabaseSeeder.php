3c672c38992992d4fc755be676a413dbcbf39775
Umair Zubair <134955307+umairkodv@users.noreply.github.com>
2025-04-08 03:43:27 +0500

    Update DatabaseSeeder.php
    DB seeder namespace added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,10 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
-         $this->call(SettingTableSeeder::class);
-         $this->call(CouponSeeder::class);
-
-
+        $this->call(UsersTableSeeder::class);
+        $this->call(SettingTableSeeder::class);
+        $this->call(CouponSeeder::class);
     }
 }
